1bd31bf932cc294fd269db5dcb28162819f9f7b8
leshe4ka46 <alex9102naid1@ya.ru>
2023-06-10 18:21:51 +0300

    upd

---
+++ front/src/components/RotatorError.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container>
+        <v-layout text-xs-center wrap>
+            <v-flex xs12 sm6 offset-sm3>
+                <v-card :color="'#FF0000'" dark>
+                    <div class="d-flex flex-no-wrap justify-space-between">
+                        <div>
+                            <v-card-title class="text-h5">
+                                {{ (text.length > 0) ? text : "Произошла ошибка при загрузке страницы" }}
+                            </v-card-title>
+                        </div>
+                    </div>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    props: ["text"],
+    name: 'RotatorError',
+    data() {
+        return {
+        }
+    }
+}
+</script>
---
+++ front/src/components/RotatorLoading.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        <v-layout text-xs-center wrap>
+            <v-flex xs12 sm6 offset-sm3>
+                <v-progress-circular :size="50" color="primary" indeterminate></v-progress-circular>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'RotatorLoading',
+    data() {
+        return {
+        }
+    }
+}
+</script>
